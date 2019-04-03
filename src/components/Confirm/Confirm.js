0c0ce0d8be7c093d00cc1a73e6cad0f2bb369ff6
Steven Chung <stevenc@wepay.com>
2019-04-02 23:16:48 -0700

    update confirm content

--- src/components/Confirm/Confirm.js
+++ src/components/Confirm/Confirm.js
@@ -5,7 +5,7 @@ const Confirm = (props) => (
   <div className={classes['Confirm']}>
     <div className={classes['ConfirmWrapper']}>
       <div className={classes['Title']}>
-        Thank you for your order!
+        Thank you for your order. You will receive an email with order confirmation and pickup instruction. 
       </div>
     </div>
   </div>
